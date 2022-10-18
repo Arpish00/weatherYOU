545215705f809a0a1e59261905c245145dc5c261
arpish <arpish.206@gmail.com>
2022-10-18 23:45:30 +0530

    looks good

---
+++ weatheryou/src/components/TopButtons.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+
+function TopButtons({setQuery}) {
+
+    const cities = [
+        {
+            id:1,
+            title:'London'
+        },
+        {
+            id:2,
+            title:'Los Angeles'
+        },
+        {
+            id:3,
+            title:'New York'
+        },
+        {
+            id:4,
+            title:'Mexico'
+        },
+        {
+            id:5,
+            title:'Tokyo'
+        },
+    ]
+  return ( 
+  <div className='flex items-center justify-around my-6'>
+    {cities.map((city) => (
+        <button key={city.id} className='text-white text-lg font-medium' onClick={() => setQuery({ q : city.title })}>
+            {city.title}
+        </button>
+    ))}
+  </div>
+  );
+}
+
+export default TopButtons;
---
+++ weatheryou/src/components/UpperLogo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function UpperLogo() {
+  return (
+    <div className='flex items-center class="shadow-xl" content-center justify-center my-6'>
+        <img src= 'https://i.postimg.cc/VNW6VQV6/Pics-Art-10-11-05-38-40.png' height={200} width={200} alt="logo" />
+    </div>
+  )
+}
+
+export default UpperLogo;
